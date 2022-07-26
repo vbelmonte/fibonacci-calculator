af296c906158dad0e8485708526856f5192b0bee
Villy Belmonte <vjbbelmont@gmail.com>
2022-07-26 01:14:01 -0700

    Add DOM functions to calculate nth Fibonacci number

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fibonacci = function(member) {
-    if (member < 0) {
-        return "OOPS";
+    if (member <= 0 || isNaN(member)) {
+        console.log("Error! Please enter a number that is greater than 0");
+        return "Error! Please enter a number that is greater than 0";
     }
+
     let i = 1;
     let base = 0;
     let nextNumber = 1;
@@ -14,3 +16,28 @@ const fibonacci = function(member) {
     }
     return nextNumber;
 };
+
+
+let getNumber = document.querySelector("#get-number");
+let getSequence = document.querySelector("#get-sequence");
+let reset = document.querySelector("#reset");
+let output = document.querySelector(".output");
+/*let input = document.querySelector("input");*/
+
+function getNthNumber() {
+    let result = calculate();
+    printOutResults(result);
+}
+
+function calculate() {
+    let result = document.querySelector("input").value;
+    return fibonacci(result);
+}
+
+function printOutResults(result) {
+    output.textContent = result;
+}
+
+getNumber.addEventListener("click", getNthNumber);
+/*getSequence.addEventListener("click", helloWorldGetSequence);
+reset.addEventListener("click", helloWorldReset);*/
